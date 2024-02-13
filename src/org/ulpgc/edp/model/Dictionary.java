d6f1603d39d390702b949f8d71d6fd64f0a5ce35
Javier <javier.castilla101@alu.ulpgc.es>
2024-02-13 16:19:45 +0000

    Dictionary base implemented and ready for develop.

--- src/org/ulpgc/edp/model/Dictionary.java
+++ src/org/ulpgc/edp/model/Dictionary.java
@@ -1,4 +1,41 @@
 package org.ulpgc.edp.model;
 
 public class Dictionary {
+    private Node[] entries;
+    private Node lastItem;
+    private int length;
+
+    public Dictionary() {
+        this.entries = new Node[11];
+    }
+
+    public Dictionary(int length) {
+        this.entries = new Node[nextPrimeNumber(length)];
+    }
+
+    public Dictionary(Object[][] items) {
+        for (int index = 0; index < items.length; index++) {
+            Object key = items[index][0];
+            Object value = items[index][1];
+            put(key, value);
+        }
+    }
+
+    public int length() {
+        return length;
+    }
+
+    private int nextPrimeNumber(int num) {
+        while (true) {
+            int counter = 0;
+            for (int i = 2; i < num; i++) {
+                if (num % i == 0) {
+                    num++;
+                    counter++;
+                    break;
+                }
+            }
+            if (counter == 0) return num;
+        }
+    }
 }
