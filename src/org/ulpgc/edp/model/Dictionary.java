d6de9dc7cb5d5a769067c3fba72ba7e23d65ee03
Javier <javier.castilla101@alu.ulpgc.es>
2024-02-13 16:40:31 +0000

    Dictioanry inner Node Class fully implemented.

--- src/org/ulpgc/edp/model/Dictionary.java
+++ src/org/ulpgc/edp/model/Dictionary.java
@@ -38,4 +38,125 @@ public class Dictionary {
             if (counter == 0) return num;
         }
     }
+
+    /**
+     * Internal and private class used to store the given pairs key - value.
+     * It has its own String representation.
+     *
+     */
+    private class Node {
+        private Object key;
+        private Object value;
+        private Node prevNode;
+        private Node nextNode;
+        private Node nextIntroducedNode;
+        private Node prevIntroducedNode;
+        private boolean isRoot;
+
+        /**
+         * Constructor of the Node Class.
+         *
+         * @param key
+         * @param value
+         * @param isRoot
+         */
+        private Node(Object key, Object value, boolean isRoot) {
+            this.key = key;
+            this.value = value;
+            this.isRoot = isRoot;
+            Dictionary.this.lastItem = this;
+        }
+
+        public Object key() {
+            return key;
+        }
+
+        public Node setKey(Object key) {
+            this.key = key;
+            return this;
+        }
+
+        public Object value() {
+            return value;
+        }
+
+        public Node setValue(Object value) {
+            this.value = value;
+            return this;
+        }
+
+        public Node prevNode() {
+            return prevNode;
+        }
+
+        public Node setPrevNode(Node prevNode) {
+            this.prevNode = prevNode;
+            return this;
+        }
+
+        public Node nextNode() {
+            return nextNode;
+        }
+
+        public Node setnextNode(Node nextNode) {
+            this.nextNode = nextNode;
+            return this;
+        }
+
+        public Node nextIntroducedNode() {
+            return nextIntroducedNode;
+        }
+
+        public Node setNextIntroducedNode(Node nextIntroducedNode) {
+            this.nextIntroducedNode = nextIntroducedNode;
+            return this;
+        }
+
+        public Node prevIntroducedNode() {
+            return prevIntroducedNode;
+        }
+
+        public Node setPrevIntroducedNode(Node prevIntroducedNode) {
+            this.prevIntroducedNode = prevIntroducedNode;
+            return this;
+        }
+
+        public boolean isRoot() {
+            return isRoot;
+        }
+
+        public void setIsRoot(boolean root) {
+            isRoot = root;
+        }
+
+        /**
+         *
+         * @param object
+         * @return whether the given Nodes key is equals to the actual one or not
+         */
+        @Override
+        public boolean equals(Object object) {
+            if (this == object) return true;
+            if (object == null || getClass() != object.getClass()) return false;
+            Node node = (Node) object;
+            return key.equals(node.key());
+        }
+
+        /**
+         *
+         * @return the representation of the Node Class
+         */
+        @Override
+        public String toString() {
+            StringBuilder str = new StringBuilder();
+            str.append(
+                    (key.getClass() == String.class) ? String.format("\'%s\'", key) : key
+            );
+            str.append(": ");
+            str.append(
+                    (value.getClass() == String.class) ? String.format("\'%s\'", value) : value
+            );
+            return str.toString();
+        }
+    }
 }
