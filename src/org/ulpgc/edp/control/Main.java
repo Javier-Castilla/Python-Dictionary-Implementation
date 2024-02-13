feb250acd52468bfcf462056909e8ec52b246d97
Javier <javier.castilla101@alu.ulpgc.es>
2024-02-13 16:17:41 +0000

    Main module implemented and ready for executing tests.

--- src/org/ulpgc/edp/control/Main.java
+++ src/org/ulpgc/edp/control/Main.java
@@ -5,8 +5,13 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 import org.ulpgc.edp.tests.*;
 
+@RunWith(Suite.class)
+@SuiteClasses({
+        TestEmptyDictionary.class
+})
+
 public class Main {
     public static void main(String[] args) {
-
+        org.junit.runner.JUnitCore.main("Main");
     }
 }
