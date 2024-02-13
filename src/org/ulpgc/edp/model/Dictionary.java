30c3d882deb4921ce6b311aaa3eae72e3115f493
Javier <javier.castilla101@alu.ulpgc.es>
2024-02-13 18:00:27 +0000

    Dictionary Class occupiedBoxes attribute implemented. Javadoc comments added where necessary.

--- src/org/ulpgc/edp/model/Dictionary.java
+++ src/org/ulpgc/edp/model/Dictionary.java
@@ -4,15 +4,29 @@ public class Dictionary {
     private Node[] entries;
     private Node lastItem;
     private int length;
+    private int occupiedBoxes;
 
+    /**
+     * Constructor by default. No length or items needed.
+     */
     public Dictionary() {
         this.entries = new Node[11];
     }
 
+    /**
+     * Constructor with initial length of the hash table specified.
+     *
+     * @param length of the initial hash table
+     */
     public Dictionary(int length) {
         this.entries = new Node[nextPrimeNumber(length)];
     }
 
+    /**
+     * Constructor given a data structure of pairs or tuples key - value to put into the new dictionary.
+     *
+     * @param items to put into the new dictionary
+     */
     public Dictionary(Object[][] items) {
         for (int index = 0; index < items.length; index++) {
             Object key = items[index][0];
@@ -21,10 +35,20 @@ public class Dictionary {
         }
     }
 
+    /**
+     *
+     * @return the number of elements in the dictionary
+     */
     public int length() {
         return length;
     }
 
+    /**
+     * Private method which calculates the next prime number of the given one.
+     *
+     * @param num to calculate the next prime number
+     * @return the next prime number
+     */
     private int nextPrimeNumber(int num) {
         while (true) {
             int counter = 0;
