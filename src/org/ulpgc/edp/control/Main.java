5ae773fc867122121a7733cbf00ef7d847c22698
Javier <javier.castilla101@alu.ulpgc.es>
2024-02-13 16:12:25 +0000

    Tests merged into master.

--- src/org/ulpgc/edp/control/Main.java
+++ src/org/ulpgc/edp/control/Main.java
@@ -1,4 +1,12 @@
 package org.ulpgc.edp.control;
 
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+import org.ulpgc.edp.tests.*;
+
 public class Main {
+    public static void main(String[] args) {
+
+    }
 }
