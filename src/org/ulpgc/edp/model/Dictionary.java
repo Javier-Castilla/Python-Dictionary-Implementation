ec0beb9d4a603c8c33026fca193e1a03d3d662de
Javier <javier.castilla101@alu.ulpgc.es>
2024-02-15 21:40:15 +0000

    LinkedList Class fully merged.

--- src/org/ulpgc/edp/model/Dictionary.java
+++ src/org/ulpgc/edp/model/Dictionary.java
@@ -1,8 +1,9 @@
 package org.ulpgc.edp.model;
 
 public class Dictionary {
-    private Node[] entries;
-    private Node lastItem;
+    private LinkedList.Node[] entries;
+    private LinkedList.Node firstItem;
+    private LinkedList.Node lastItem;
     private int length;
     private int occupiedBoxes;
 
@@ -10,7 +11,7 @@ public class Dictionary {
      * Constructor by default. No length or items needed.
      */
     public Dictionary() {
-        this.entries = new Node[11];
+        this.entries = new LinkedList.Node[11];
     }
 
     /**
@@ -19,7 +20,7 @@ public class Dictionary {
      * @param length of the initial hash table
      */
     public Dictionary(int length) {
-        this.entries = new Node[nextPrimeNumber(length)];
+        this.entries = new LinkedList.Node[nextPrimeNumber(length)];
     }
 
     /**
@@ -60,169 +61,6 @@ public class Dictionary {
                 }
             }
             if (counter == 0) return num;
-        }
-    }
-
-    public Object pop(Object key){
-        int index = hash(key);
-        Node nodo = entries[index];
-        if(nodo.key().equals(key)){
-            if(nodo.nextNode() == null) {
-                entries[index] = null;
-            } else {
-                entries[index] = nodo.nextNode();
-            }
-            return nodo.value();
-        }
-
-        while(nodo.nextNode() != null && nodo.key() != key){
-            nodo = nodo.nextNode();
-        }
-
-        if(nodo.nextNode() == null){
-            nodo.prevNode().setnextNode(null);
-        } else {
-            nodo.prevNode().setnextNode(nodo.nextNode());
-            nodo.nextNode().setPrevNode(nodo.prevNode());
-        }
-
-        if(nodo.nextIntroducedNode() == null){
-            nodo.prevIntroducedNode().setNextIntroducedNode(null);
-        } else {
-            nodo.prevIntroducedNode().setNextIntroducedNode(nodo.nextIntroducedNode());
-            nodo.nextIntroducedNode().setPrevIntroducedNode(nodo.prevIntroducedNode());
-        }
-
-        length -= 1;
-        return nodo.value();
-    }
-
-    public void put(Object key, Object value){
-        int index = hash(key);
-        Node nodo = entries[index];
-        while(nodo.nextNode() != null && nodo.key().equals(key)){
-            entries[index] = nodo.nextNode();
-
-        }
-    }
-    /**
-     * Internal and private class used to store the given pairs key - value.
-     * It has its own String representation.
-     *
-     */
-    private class Node {
-        private Object key;
-        private Object value;
-        private Node prevNode;
-        private Node nextNode;
-        private Node nextIntroducedNode;
-        private Node prevIntroducedNode;
-        private boolean isRoot;
-
-        /**
-         * Constructor of the Node Class.
-         *
-         * @param key
-         * @param value
-         * @param isRoot
-         */
-        private Node(Object key, Object value, boolean isRoot) {
-            this.key = key;
-            this.value = value;
-            this.isRoot = isRoot;
-            Dictionary.this.lastItem = this;
-        }
-
-        public Object key() {
-            return key;
-        }
-
-        public Node setKey(Object key) {
-            this.key = key;
-            return this;
-        }
-
-        public Object value() {
-            return value;
-        }
-
-        public Node setValue(Object value) {
-            this.value = value;
-            return this;
-        }
-
-        public Node prevNode() {
-            return prevNode;
-        }
-
-        public Node setPrevNode(Node prevNode) {
-            this.prevNode = prevNode;
-            return this;
-        }
-
-        public Node nextNode() {
-            return nextNode;
-        }
-
-        public Node setnextNode(Node nextNode) {
-            this.nextNode = nextNode;
-            return this;
-        }
-
-        public Node nextIntroducedNode() {
-            return nextIntroducedNode;
-        }
-
-        public Node setNextIntroducedNode(Node nextIntroducedNode) {
-            this.nextIntroducedNode = nextIntroducedNode;
-            return this;
-        }
-
-        public Node prevIntroducedNode() {
-            return prevIntroducedNode;
-        }
-
-        public Node setPrevIntroducedNode(Node prevIntroducedNode) {
-            this.prevIntroducedNode = prevIntroducedNode;
-            return this;
-        }
-
-        public boolean isRoot() {
-            return isRoot;
-        }
-
-        public void setIsRoot(boolean root) {
-            isRoot = root;
-        }
-
-        /**
-         *
-         * @param object
-         * @return whether the given Nodes key is equals to the actual one or not
-         */
-        @Override
-        public boolean equals(Object object) {
-            if (this == object) return true;
-            if (object == null || getClass() != object.getClass()) return false;
-            Node node = (Node) object;
-            return key.equals(node.key());
-        }
-
-        /**
-         *
-         * @return the representation of the Node Class
-         */
-        @Override
-        public String toString() {
-            StringBuilder str = new StringBuilder();
-            str.append(
-                    (key.getClass() == String.class) ? String.format("\'%s\'", key) : key
-            );
-            str.append(": ");
-            str.append(
-                    (value.getClass() == String.class) ? String.format("\'%s\'", value) : value
-            );
-            return str.toString();
         }
     }
 }
